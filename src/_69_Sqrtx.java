8200f9cda09370c5f53e9f486197724432f0e304
Yogaa <yujia_jjzhang@126.com>
2024-11-14 17:19:13 +0900

    update the plan

---
+++ src/_69_Sqrtx.java
@@ -0,0 +1,10 @@
+
+public class _69_Sqrtx {
+	public int mySqrt(int x) {
+		return 0;
+	}
+	public static void main(String[] args) {
+		_69_Sqrtx solution = new _69_Sqrtx();
+		System.out.println(solution.mySqrt(0));
+	}
+}
